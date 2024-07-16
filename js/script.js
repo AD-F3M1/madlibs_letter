8136a3ed11b82111d47fc927d505eda69a151bb1
Akitobi Oluwafemi <akitobioluwafemi@gmail.com>
2024-07-16 14:23:44 +0100

    add index code and a little of javascript code for the letter

---
+++ js/script.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+    
+    $("#formone").submit(function(refresh) {
+        
+        refresh.preventDefault();
+
+        const inputtedName = $("#name").val();
+        const inputtedDate = $("#date").val();
+        const inputtedRecipientName = $("#recipientName").val();
+        const inputtedTitle = $("#title").val();
+        const inputtedAddress = $("#address").val();
+
+        $(".name").text(inputtedName);
+        $(".date").text(inputtedDate);
+        $(".recipientName").text(inputtedRecipientName);
+        $(".title").text(inputtedTitle);
+        $(".address").text(inputtedAddress);
+
+        $("#letter").show();
+
+        $()
+    })
+})
